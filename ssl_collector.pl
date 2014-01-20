d418b8a6f6bae2bd0a6286039b75ca3c961a9b66
Julien Legras <julien.legras@etu.univ-rouen.fr>
2014-01-20 17:41:14 +0100

    MAJ ssl collector : multi threads, status timer, nouvelles adresses de test

--- ssl_collector.pl
+++ ssl_collector.pl
@@ -1,6 +1,18 @@
 #!/usr/bin/perl
 
 use Term::ANSIColor qw(:constants);
+use threads;
+use Thread::Semaphore;
+use POSIX 'strftime';
+use Time::HiRes qw ( setitimer ITIMER_REAL time );
+
+if ($#ARGV != 0) {
+	print "Usage: ssl_collector.pl <addresses_file>\n";
+	exit;
+}
+
+my $start = time;
+my $sem_nb = 10;
 
 open (ADDR, $ARGV[0]);
 
@@ -12,22 +24,67 @@ unless ( -d "certs" ) {
 	system "mkdir certs";
 }
 
-while (<ADDR>) {
-	chomp;
-	print "Fetching $_...\n";
-	system "echo \"\" | openssl s_client -connect $_:443 -sess_out tmp.pem > /dev/null 2> /dev/null";
-	if ($? != 0) {
-		print RED, "Something wrong happened with $_\n",RESET;
-		next;
-	}
-	print "Storing certificate...\n";
-	system "openssl sess_id -in tmp.pem -cert > certs/$_.pem";
-	print "Storing key...\n";
-	system "openssl x509 -in certs/$_.pem -noout -pubkey > keys/$_.pem";
-	print GREEN,"$_ done!\n",RESET;
+unless ( -d "logs" ) {
+	system "mkdir logs";
 }
 
+my $sem : shared = Thread::Semaphore->new($sem_nb);
+
+my $addr_total = `wc -l $ARGV[0] | cut -d' ' -f1`;
+$addr_total =~ s/\n//g;
+
+my $addr_nb : shared = 0;
+my $failed_nb : shared = 0;
+
+sub show_status {
+	my $duration = time - $start;
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime $duration;
+  my $hour = $hour - 1;
+	my $pct = sprintf ("%0.2f", ($addr_nb * 100) / $addr_total);
+	print "$addr_nb/$addr_total ($pct %) in $hour h $min m $sec s  -- $failed_nb failed\n";
+};
+
+$SIG{ALRM} = \&show_status;
+setitimer(ITIMER_REAL, 2, 5);
+
+while (<ADDR>) {
+	chomp;
+	$sem->down;
+	threads->create (sub {
+        my ($addr) = @_;
+					system "echo \"\" | timeout 20 openssl s_client -connect $addr:443 -sess_out tmp_$addr.pem -ignore_critical -showcerts -CApath /etc/ssl/certs > $addr.log 2> logs/$addr.log ";
+					if ($? != 0) {
+            #print RED, "Something wrong happened with $addr, see $addr.log for more details\n", RESET;
+						$failed_nb += 1;
+					} else {
+						system "openssl sess_id -in tmp_$addr.pem -cert > certs/$addr.pem 2>> logs/$addr.log";
+						if ($? != 0) {
+							#print RED, "Something wrong happened with $addr, see $addr.log for more details\n", RESET;
+						} else {
+							system "openssl x509 -in certs/$addr.pem -noout -pubkey > keys/$addr.pem 2>> logs/$addr.log";	
+							if ($? != 0) {
+								#print RED, "Something wrong happened with $addr, see $addr.log for more details\n", RESET;
+							}
+						}
+					}
+				$addr_nb += 1;
+				system "rm -rf tmp_$addr.pem";
+				$sem->up;
+	}, $_);
+}
+
+#$sem->down($sem_nb);
 if ( -e "tmp.pem" ) {
 	system "rm tmp.pem";
 }
 close (ADDR);
+
+foreach $thr (threads->list) { 
+        # Don't join the main thread or ourselves 
+        if ($thr->tid && !threads::equal($thr, threads->self)) { 
+            $thr->join; 
+        } 
+    }
+
+show_status();
+print "See logs file for more details\n";
